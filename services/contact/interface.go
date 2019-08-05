f721b70734b4e69c9d89cffb642dcf1bebed69cc
Chidambaram <chidam1994@gmail.com>
2019-08-06 01:17:57 +0530

    Repository interface added, to remove data storage dependency.

---
+++ services/contact/interface.go
@@ -0,0 +1,17 @@
+package contactsvc
+
+import (
+	"github.com/chidam1994/happyfox/models"
+	"github.com/google/uuid"
+)
+
+type Repository interface {
+	Save(contact *models.Contact) (uuid.UUID, error)
+	AddEmail(contactId uuid.UUID, email *models.Email) error
+	AddPhNum(contactId uuid.UUID, phNum *models.PhNum) error
+	RemEmail(contactId uuid.UUID, email *models.Email) error
+	RemPhNum(contactId uuid.UUID, email *models.PhNum) error
+	Delete(contactId uuid.UUID) error
+	Find(filterMap map[string]string) ([]*models.Contact, error)
+	FindById(contactId uuid.UUID) (*models.Contact, error)
+}
